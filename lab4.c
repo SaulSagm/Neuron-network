5131e00d2b9f334b5a91a515e608d9c55f691d88
Saul Gomez <saul_goma@hotmail.com>
2019-04-23 18:19:13 -0500

    Update lab4.c

--- lab4.c
+++ lab4.c
@@ -118,7 +118,7 @@ int main(void){
                 n++;                    //it'll sum 
         }
         epoch++;
-    }while(n!=trainSet && epoch<=300);    //It'll stop when the sum is equal to the train Set orwhen it had
+    }while(n!=trainSet && epoch<=250);    //It'll stop when the sum is equal to the train Set orwhen it had
                                                               //reached a certain number of repetition so it won't be trapped
     if(epoch>=300 && n!=trainSet)         //in an infinite loop
     {
