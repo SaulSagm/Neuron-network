9f964b434fa5fbcad217308058c6f3133cab5557
Saul Gomez <saul_goma@hotmail.com>
2019-04-23 18:34:41 -0500

    Update lab4.c

--- lab4.c
+++ lab4.c
@@ -118,9 +118,9 @@ int main(void){
                 n++;                    //it'll sum 
         }
         epoch++;
-    }while(epoch!=200);    //It'll stop when the sum is equal to the train Set orwhen it had
-                                                              //reached a certain number of repetition so it won't be trapped
-    if(n!=trainSet || epoch==200)         //in an infinite loop
+    }while(n!=trainSet && epoch!=(trainSet^trainSet));    //It'll stop when the sum is equal to the train Set orwhen it had
+                                                          //reached a certain number of repetition so it won't be trapped
+    if(epoch==(trainSet^trainSet) && n!=trainSet)         //in an infinite loop
     {
         printf("no solution found\n");
     }
@@ -141,3 +141,4 @@ int main(void){
     return 0;
 
 }
+
